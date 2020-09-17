eb56f468ab63dac564ca336598271c243656a434
andysml <andy922200@gmail.com>
2020-09-17 20:37:34 +0800

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Movies from "./../views/Movies.vue";
+import NotFound from "./../views/NotFound404.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Movies",
     component: Movies
+  },
+  {
+    path: "*",
+    name: "not-found",
+    component: NotFound
   }
 ];
 
---
+++ src/views/NotFound404.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container pa-5">
+    <div id="for404">
+      <div class="logo-404 half-area">
+        <img src="../assets/404.png" alt="404" />
+      </div>
+      <div class="content half-area">
+        <h3>Oops... 找不到網頁</h3>
+        <p>如果不支援自動跳轉，請點選下方按鈕</p>
+        <router-link to="/">
+          <el-button type="info" size="medium">To Homepage</el-button>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorPage",
+  created() {
+    setTimeout(() => this.$router.push({ path: "/" }), 5000);
+  }
+};
+</script>
+
+<style lang="scss" scoped src="./../styles/NotFound404.scss"></style>
